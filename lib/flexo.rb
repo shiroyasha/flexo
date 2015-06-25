932d16c979b2d9d5de21a415bbf16904a3fc66b7
Igor Sarcevic <igisar@gmail.com>
2015-06-25 19:06:57 +0200

    Put Flexo::DataSet in its own file

--- lib/flexo.rb
+++ lib/flexo.rb
@@ -1,29 +1,6 @@
 require "flexo/version"
 
 module Flexo
+  require "flexo/data_set"
   require "flexo/histogram"
-
-  class DataSet
-    require "flexo/data_set/means"
-    require "flexo/data_set/extremes"
-    require "flexo/data_set/histograms"
-    require "flexo/data_set/aggregations"
-    require "flexo/data_set/dispersion"
-
-    include Aggregations
-    include Extremes
-    include Means
-    include Histograms
-    include Dispersion
-
-    attr_reader :data
-
-    def initialize(data)
-      @data = data.map(&:to_f).freeze
-    end
-
-    def size
-      @data.length.to_f
-    end
-  end
 end
---
+++ lib/flexo/data_set.rb
@@ -0,0 +1,23 @@
+class Flexo::DataSet
+  require "flexo/data_set/means"
+  require "flexo/data_set/extremes"
+  require "flexo/data_set/histograms"
+  require "flexo/data_set/aggregations"
+  require "flexo/data_set/dispersion"
+
+  include Aggregations
+  include Extremes
+  include Means
+  include Histograms
+  include Dispersion
+
+  attr_reader :data
+
+  def initialize(data)
+    @data = data.map(&:to_f).freeze
+  end
+
+  def size
+    @data.length.to_f
+  end
+end
